e081577961bd82e18487da22f54debeb927ab5a3
vishnu prakash <vishnuprakaash257@gmail.com>
2021-11-19 23:10:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Vishnu from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h3>Hello world!</h3>
+      <Vishnu></Vishnu>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem(){
+    return <h1>Vishnu Prakash</h1>
+}
+
+export default ExpenseItem;
