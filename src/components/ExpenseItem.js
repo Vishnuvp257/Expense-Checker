d73b00fe43883030287db4b352dc64acc2fc0d96
vishnu prakash <vishnuprakaash257@gmail.com>
2021-11-28 09:59:17 +0530

    split Expenses into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem(){
-    return <h1>Vishnu Prakash</h1>
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  // console.log(props.data.date);
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.data.date} />
+      <div className="expense-item__description">
+        <h2>{props.data.title}</h2>
+        <div className="expense-item__price">{props.data.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
